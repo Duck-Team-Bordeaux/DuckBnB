7437ca0d4d49aa37982f69fcb15f459580dbc6c7
Armi Piamonte <armi.piamonte@gmail.com>
2023-08-21 16:50:54 +0200

    [ADD]-model duck

---
+++ db/migrate/20230821144842_create_ducks.rb
@@ -0,0 +1,10 @@
+class CreateDucks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ducks do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
