17ee9836292f4906b2a7b6dc868b8fee9ddad4b2
Armi Piamonte <armi.piamonte@gmail.com>
2023-08-22 12:33:39 +0200

    [ADDED]-Users model with all custom fields, with controllers and views by Devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -58,6 +59,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
