9c6c73b918a87ae7e9daa5b69cab9b55182ce2c4
Armi Piamonte <armi.piamonte@gmail.com>
2023-08-24 16:24:07 +0200

    [UPDATE]-Registration form updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
     @hide_navbar = true
     @hide_footer = true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   has_many :bookings
   has_many :favorites
 end
