92dc041b93db9e6088775440e5b9dedfc6c7c7bd
Natheo Brault <nath.brault.pro@gmail.com>
2023-08-24 10:57:56 +0200

    [UPDATE] - DucksController

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,13 +1,11 @@
 class DucksController < ApplicationController
-  before_action :set_duck, only: %i[show create edit update destroy]
+  before_action :set_duck, only: %i[show edit update destroy]
 
   def index
     @ducks = Duck.all
   end
 
-  def show
-    @duck = Duck.find(params[:id])
-  end
+  def show() end
 
   def new
     @duck = Duck.new
@@ -22,12 +20,14 @@ class DucksController < ApplicationController
     end
   end
 
-  def edit
-
-  end
+  def edit() end
 
   def update
-    @duck.update(duck_params)
+    if @duck.update(duck_params)
+      redirect_to duck_path(@duck)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -38,7 +38,7 @@ class DucksController < ApplicationController
   private
 
   def duck_params
-    params.require(:ducks).permit(:name, :description, :price, :category, :height, :width, :depth)
+    params.require(:ducks).permit(:name, :description, :url, :price, :category)
   end
 
   def set_duck
