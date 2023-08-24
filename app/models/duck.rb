8574030df4f1a1bf0b24a88aa5c6f6cea60b4913
Armi Piamonte <armi.piamonte@gmail.com>
2023-08-24 17:17:11 +0200

    [UPDATE]-remove description validation on duck

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -3,6 +3,6 @@ class Duck < ApplicationRecord
   has_many :favorites
   belongs_to :user
 
-  validates :name, :description, :url, :price, :category, presence: true
+  validates :name, :url, :price, :category, presence: true
   validates :name, uniqueness: true
 end
